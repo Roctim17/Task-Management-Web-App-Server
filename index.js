9bc0fb0d3e8de0db46ab33dcca822bb6b243583a
Roctim17 <mdrokibulislamroctim@gmail.com>
2022-08-13 22:50:10 +0600

    basic setup

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9conth7.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("data")
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send("hello Brain Station")
+});
+
+app.listen(port, () => {
+    console.log(port)
+})
