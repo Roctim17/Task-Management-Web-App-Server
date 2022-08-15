3b159a906a9067ec0844e30e62a0e21cee2cff56
Roctim17 <mdrokibulislamroctim@gmail.com>
2022-08-16 03:51:53 +0600

    done

--- index.js
+++ index.js
@@ -21,52 +21,96 @@ async function run() {
 
         // get api
 
-        app.get('/member', async (req, res) => {
-            const query = {};
-            const cursor = memberCollection.find(query);
-            const members = await cursor.toArray();
-            res.send(members)
-        })
+
+        // get task of all user
         app.get("/createTask", async (req, res) => {
             const task = await taskCollection.find({}).toArray();
             res.send(task);
         });
 
+        // get task with particular user
         app.get('/createTask/:email', async (req, res) => {
             const email = req.params.email;
             const query = { email: email };
             const tasks = await taskCollection.find(query).toArray();
             res.send(tasks);
         });
+        //get member to all user
+        app.get('/member', async (req, res) => {
+            const query = {};
+            const cursor = memberCollection.find(query);
+            const members = await cursor.toArray();
+            res.send(members)
+        })
+
+        // get member with particular user
+        app.get('/member/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const cursor = memberCollection.find(query);
+            const members = await cursor.toArray();
+            res.send(members)
+        })
+
+        // get task with id api
+        app.get("/createTask/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const task = await taskCollection.find(query).toArray();
+            res.send(task);
+        });
+        // get task details with single
+        app.get("/taskDetails/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const task = await taskCollection.findOne(query);
+            res.send(task);
+        });
+        // get member details with single
+        app.get("/memberDetails/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const task = await memberCollection.findOne(query);
+            res.send(task);
+        });
+        // get member with id api
+        app.get("/member/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const task = await memberCollection.find(query).toArray();
+            res.send(task);
+        });
+
+
 
         // Post Api 
         //  Add new task api 
-        app.post('/createTask/:email', async (req, res) => {
+        app.post('/createTask', async (req, res) => {
             const data = req.body;
             const result = await taskCollection.insertOne(data);
             return res.send(result);
         })
 
-        app.put("/updatedTask/:id", async (req, res) => {
+        // Update Task details
+        app.put("/createTask/:id", async (req, res) => {
             const id = req.params.id;
             const data = req.body;
             const filter = { _id: ObjectId(id) };
-            const option = { upsert: true };
             const updatedData = {
                 $set: data,
             };
-            const result = await taskCollection.updateOne(filter, updatedData, option);
+            const result = await taskCollection.updateOne(filter, updatedData);
             res.send(result);
         });
+        // Update member details
         app.put("/member/:id", async (req, res) => {
             const id = req.params.id;
             const data = req.body;
             const filter = { _id: ObjectId(id) };
-            const option = { upsert: true };
             const updatedData = {
                 $set: data,
             };
-            const result = await memberCollection.updateOne(filter, updatedData, option);
+            const result = await memberCollection.updateOne(filter, updatedData);
             res.send(result);
         });
 
